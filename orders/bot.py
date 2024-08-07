6d0b9138f2a7161bddacecff91a2dfdc4839c3db
MasterVVK <vvk@2140101.ru>
2024-08-07 21:18:00 +0500

    Update bot.py

--- orders/bot.py
+++ orders/bot.py
@@ -2,66 +2,66 @@ import json
 import os
 import logging
 import django
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.strategy import FSMStrategy
+from aiohttp import web
+
 from orders.models import Product, Order, OrderProduct
 
 # Установка переменной окружения для настройки Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flower_delivery.settings')
 django.setup()
 
+# Чтение конфигурационного файла
 with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'config.json')) as config_file:
     config = json.load(config_file)
 
 API_TOKEN = config['telegram_token']
-WEBHOOK_URL = config['webhook_url']
+WEBHOOK_HOST = config['webhook_url']
+WEBHOOK_PATH = '/webhook/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+CHAT_ID = config['chat_id']
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
+# Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(storage=storage, fsm_strategy=FSMStrategy(user=True))
+dp = Dispatcher(storage=MemoryStorage(), fsm_strategy=FSMStrategy.USER_IN_CHAT)
 
-# Настройка middleware для логирования
-dp.update.middleware(logging)
+# Функция для отправки уведомления о новом заказе
+async def send_new_order_notification(order):
+    message = f"Новый заказ #{order.id} от {order.user.username}:\n"
+    for order_product in order.orderproduct_set.all():
+        message += f"{order_product.quantity} x {order_product.product.name}\n"
+    await bot.send_message(chat_id=CHAT_ID, text=message)
 
-@dp.message(Command("start"))
-async def send_welcome(message: Message):
-    await message.answer("Привет! Я бот для заказа цветов. Вы можете посмотреть наш каталог и сделать заказ.")
+# Обработчики команд
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я бот для управления заказами.")
 
-@dp.message(Command("catalog"))
-async def send_catalog(message: Message):
+@dp.message_handler(commands=['catalog'])
+async def send_catalog(message: types.Message):
     products = Product.objects.all()
     response = "Каталог продуктов:\n"
     for product in products:
         response += f"{product.name} - {product.price} руб.\n"
-    await message.answer(response)
+    await message.reply(response)
 
-@dp.message(Command("order"))
-async def create_order(message: Message):
-    args = message.text.split()[1:]
-    if len(args) != 2:
-        await message.answer("Используйте команду в формате /order <product_id> <quantity>")
-        return
-    product_id, quantity = map(int, args)
-    product = Product.objects.get(id=product_id)
-    order = Order(user_id=message.from_user.id)
-    order.save()
-    order_product = OrderProduct(order=order, product=product, quantity=quantity)
-    order_product.save()
-    await message.answer(f"Заказ на {quantity} x {product.name} успешно создан!")
+# Настройки вебхука
+async def on_startup(app: web.Application):
+    await bot.set_webhook(WEBHOOK_URL)
 
-async def notify_new_order(order):
-    message = f"Новый заказ №{order.id}\nПользователь: {order.user.username}\nСтатус: {order.get_status_display()}\n"
-    message += "Продукты:\n"
-    for order_product in order.orderproduct_set.all():
-        message += f"{order_product.quantity} x {order_product.product.name}\n"
-    await bot.send_message(chat_id=YOUR_CHAT_ID, text=message)
+async def on_shutdown(app: web.Application):
+    await bot.delete_webhook()
+
+# Создание и настройка веб-приложения
+app = web.Application()
+app.on_startup.append(on_startup)
+app.on_shutdown.append(on_shutdown)
+dp.setup(app)
 
 if __name__ == '__main__':
-    from aiogram import executor
-    executor.start_polling(dp, skip_updates=True)
+    web.run_app(app, host='0.0.0.0', port=5000)
