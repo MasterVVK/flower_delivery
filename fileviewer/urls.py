9ce50a3c0967b109bbce97443a96b9dae79f4bf5
MasterVVK <vvk@2140101.ru>
2024-08-09 13:22:44 +0500

    Up

--- fileviewer/urls.py
+++ fileviewer/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.list_files, name='list_files'),
+    path('<path:path>/', views.list_files, name='list_files'),
     path('view/<path:path>/', views.view_file, name='view_file'),
 ]
