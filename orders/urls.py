e4a0b0514dfaa7d02834c044a6a089f3d1b2e91e
MasterVVK <vvk@2140101.ru>
2024-08-12 23:20:41 +0500

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('manage_products/', views.manage_products, name='manage_products'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
     path('', views.product_list, name='product_list'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),
     path('order/create/<int:product_id>/', views.order_create, name='order_create'),
