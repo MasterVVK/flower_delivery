39515581254c29bd45baabbb24fe863325f246ce
MasterVVK <vvk@2140101.ru>
2024-08-12 16:11:19 +0500

    Up

--- flower_delivery/urls.py
+++ flower_delivery/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('fileviewer/', include('fileviewer.urls')),
     path('users/', include('users.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('orders.urls')),  # Маршруты для приложения orders
 ]
