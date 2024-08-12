7fcb6ed2e7bcef8f2aa9ad7bae6aafebab6a909f
MasterVVK <vvk@2140101.ru>
2024-08-12 19:28:39 +0500

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,9 +1,18 @@
-# users/models.py
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('Admin', 'Администратор'),
+        ('Manager', 'Менеджер'),
+        ('User', 'Пользователь'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='User')
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
     groups = models.ManyToManyField(
         'auth.Group',
         related_name='customuser_set',  # Измените related_name здесь
@@ -18,3 +27,6 @@ class CustomUser(AbstractUser):
         help_text='Specific permissions for this user.',
         verbose_name='user permissions',
     )
+
+    def __str__(self):
+        return self.username
