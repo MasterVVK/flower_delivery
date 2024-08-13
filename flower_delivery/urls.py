dc2a6dc677efda4d214d95de8ca171029550ec8b
MasterVVK <vvk@2140101.ru>
2024-08-13 18:34:50 +0500

    Update urls.py

--- flower_delivery/urls.py
+++ flower_delivery/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('orders.urls')),  # Маршруты для приложения orders
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
