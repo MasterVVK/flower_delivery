aaca86bffa575cccb4e4b7a38906339c01eaea99
MasterVVK <vvk@2140101.ru>
2024-08-15 00:11:18 +0500

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Product, ProductCategory, Order, Review
 from .forms import ProductForm, ProductCategoryForm, OrderForm, ReviewForm
+from django.urls import reverse  # Добавьте этот импорт
 from django.core.paginator import Paginator
 from django.http import JsonResponse
 
