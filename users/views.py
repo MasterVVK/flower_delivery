5cc99078d0de00548b81602bece7e38b0410a581
MasterVVK <vvk@2140101.ru>
2024-08-17 22:28:11 +0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ from django.contrib.sessions.models import Session
 
 import logging
 
-#logger = logging.getLogger(__name__)
+logger = logging.getLogger('my_custom_logger')
 
 def register(request):
     if request.method == 'POST':
@@ -25,7 +25,6 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
-
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -33,23 +32,20 @@ def login_view(request):
             user = form.get_user()
             session_key = request.session.session_key
 
-            # Логгирование текущей сессии
-            logging.info(f"Session key before login: {session_key}")
+            logger.info(f"Session key before login: {session_key}")
 
             guest_cart_items = []
             if session_key:
                 try:
                     guest_cart = Cart.objects.get(session_key=session_key)
                     guest_cart_items = [{'product_id': item.product.id, 'quantity': item.quantity} for item in guest_cart.items.all()]
-                    logging.info(f"Guest cart items: {guest_cart_items}")
+                    logger.info(f"Guest cart items: {guest_cart_items}")
                 except Cart.DoesNotExist:
-                    logging.info("Guest cart does not exist")
+                    logger.info("Guest cart does not exist")
 
-            # Логиним пользователя
             login(request, user)
 
-            # Логгирование новой сессии
-            logging.info(f"Session key after login: {request.session.session_key}")
+            logger.info(f"Session key after login: {request.session.session_key}")
 
             if guest_cart_items:
                 user_cart, created = Cart.objects.get_or_create(user=user)
@@ -59,17 +55,19 @@ def login_view(request):
                     if not created:
                         cart_item.quantity += item['quantity']
                     cart_item.save()
-                logging.info(f"User cart items after merging: {list(user_cart.items.all())}")
+                logger.info(f"User cart items after merging: {list(user_cart.items.all())}")
 
-            # Получаем и логируем next_url
-            next_url = request.POST.get('next') or request.GET.get('next') or 'index'
-            logging.debug(f"Redirecting to: {next_url}")
+            # Если параметр next пустой, установим его значение на страницу заказа
+            next_url = request.POST.get('next') or request.GET.get('next')
+            if not next_url:
+                next_url = 'checkout'  # Задаем значение по умолчанию
+
+            logger.info(f"Redirecting to: {next_url}")
 
             return redirect(next_url)
     else:
         form = AuthenticationForm()
     return render(request, 'users/login.html', {'form': form})
-
 
 
 @login_required
