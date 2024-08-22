d7f3ba8a5b7264e38c4be1e9820530184851eb31
MasterVVK <vvk@2140101.ru>
2024-08-22 15:46:28 +0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -19,7 +19,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('product_list')
+            return redirect('index')
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
